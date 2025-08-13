571a489b0e79a61bb3d4a9ca63c89eaa2417ea24
Sepehr <sepehr.goodarzi@gmail.com>
2025-08-13 22:10:32 +1000

    Implement the polls application with models, views, templates, and admin configuration. Added functionality for creating, voting on, and viewing poll results.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,48 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """Display all available polls"""
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    """Display a specific poll with voting options"""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    """Display poll results"""
+    question = get_object_or_404(Question, pk=question_id)
+    total_votes = sum(choice.votes for choice in question.choices.all())
+    context = {
+        "question": question,
+        "total_votes": total_votes,
+    }
+    return render(request, "polls/results.html", context)
+
+
+def vote(request, question_id):
+    """Process a vote submission"""
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form with error message
+        messages.error(request, "You didn't select a choice.")
+        return render(request, "polls/detail.html", {"question": question})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        messages.success(request, "Your vote has been recorded!")
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
